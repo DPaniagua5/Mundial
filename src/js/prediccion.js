ca75a6f51a6152d475c023a8293d4018e9f01aaa
DPaniagua5 <davidpaniaguaja1@gmail.com>
2022-11-30 21:02:10 -0600

    brackets de calificación

--- src/js/prediccion.js
+++ src/js/prediccion.js
@@ -1,9 +1,14 @@
-const items = document.getElementsByClassName("Equipo");  
+fetch("https://apiv2.allsportsapi.com/football/?&met=Topscorers&leagueId=28&APIkey=19f0c81f8b64991ae9a72bcd9b2b83710b7564c139eaea6f8e08958fd6114099",{
+	"method":"GET",
+	"headers":{
+	}
+})
 
-for(var i = 0; i<items.length;i++){
-    console.log(items[i]);
+let pokedex = require('response.json');
+res.send(pokedex);
+
+if("success" == 1){
+	console.log("y")
+}else{
+	console.log("n")
 }
-
-addEventListener({
-
-});
